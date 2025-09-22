8e2c6868d0117a6833c4c3282870365ac83c707d
user <vikapetrova8@gmail.com>
2025-09-22 15:41:19 +0300

    KAN-12 feat: add Textarea component

---
+++ src/shared/components/Textarea/Textarea.stories.tsx
@@ -0,0 +1,52 @@
+import { Meta, StoryObj } from '@storybook/nextjs-vite'
+import { fn } from 'storybook/test'
+import { Textarea } from '@/shared/components/Textarea/'
+
+const meta = {
+   title: 'Components/Textarea',
+   component: Textarea,
+   tags: ['autodocs'],
+   parameters: {
+      layout: 'centered',
+   },
+   globals: {
+      backgrounds: { value: 'dark' },
+   },
+   args: {
+      label: 'Text-area',
+      placeholder: 'Text-area',
+      onValueChange: fn(),
+   },
+} satisfies Meta<typeof Textarea>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
+
+export const Active: Story = {
+   args: {
+      defaultValue: 'Some text',
+      className: 'border-light-100',
+   },
+}
+
+export const Invalid: Story = {
+   args: {
+      defaultValue: 'Some text',
+      errorMessage: 'Error text',
+   },
+}
+
+export const Focus: Story = {
+   args: {
+      defaultValue: 'Some text',
+      autoFocus: true,
+   },
+}
+
+export const Disabled: Story = {
+   args: {
+      disabled: true,
+   },
+}
