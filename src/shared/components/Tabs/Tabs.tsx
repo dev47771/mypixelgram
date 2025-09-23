59a9dae04c9359abcb3e7fda3ab4505f4cb8626e
Evgeniy Ivanov <p8635241@gmail.com>
2025-09-24 01:24:45 +0500

    KAN-13 fix: tabstrigger styles fix

--- src/shared/components/Tabs/Tabs.tsx
+++ src/shared/components/Tabs/Tabs.tsx
@@ -1,26 +1,66 @@
 import type { ComponentPropsWithRef } from 'react'
 import * as TabsPrimitive from '@radix-ui/react-tabs'
+import { clsx } from 'clsx'
 
-const Tabs = TabsPrimitive.Root
+const Tabs = ({
+   className,
+   children,
+   ...rest
+}: ComponentPropsWithRef<typeof TabsPrimitive.Root>) => {
+   return (
+      <TabsPrimitive.Root className={clsx('', className)} {...rest}>
+         {children}
+      </TabsPrimitive.Root>
+   )
+}
+Tabs.displayName = TabsPrimitive.Root.displayName
 
-const TabsList = ({ children, ...rest }: ComponentPropsWithRef<typeof TabsPrimitive.List>) => {
-   return <TabsPrimitive.List {...rest}>{children}</TabsPrimitive.List>
+const TabsList = ({
+   className,
+   children,
+   ...rest
+}: ComponentPropsWithRef<typeof TabsPrimitive.List>) => {
+   return (
+      <TabsPrimitive.List className={clsx('mb-6 flex w-full justify-between', className)} {...rest}>
+         {children}
+      </TabsPrimitive.List>
+   )
 }
 TabsList.displayName = TabsPrimitive.List.displayName
 
 const TabsTrigger = ({
    children,
+   className,
    ...rest
 }: ComponentPropsWithRef<typeof TabsPrimitive.Trigger>) => {
-   return <TabsPrimitive.Trigger {...rest}>{children}</TabsPrimitive.Trigger>
+   return (
+      <TabsPrimitive.Trigger
+         className={clsx(
+            'leading-m text-m border-b-dark-300 text-dark-100 w-1/4 cursor-pointer border-b py-1 font-semibold',
+
+            'hover:[&:not([data-state=active]):not([data-disabled])]:bg-accent-100',
+            'hover:data-[state=active]:[&:not([data-disabled])]:bg-accent-100',
+            'data-[state=active]:text-accent-500 data-[state=active]:border-b-accent-500',
+            className
+         )}
+         {...rest}
+      >
+         {children}
+      </TabsPrimitive.Trigger>
+   )
 }
 TabsTrigger.displayName = TabsPrimitive.Trigger.displayName
 
 const TabsContent = ({
    children,
+   className,
    ...rest
 }: ComponentPropsWithRef<typeof TabsPrimitive.Content>) => {
-   return <TabsPrimitive.Content {...rest}>{children}</TabsPrimitive.Content>
+   return (
+      <TabsPrimitive.Content className={clsx('', className)} {...rest}>
+         {children}
+      </TabsPrimitive.Content>
+   )
 }
 TabsContent.displayName = TabsPrimitive.Content.displayName
 
