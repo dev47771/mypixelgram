059f5ee7be1c7055ee29121d2937b057dfd65303
Darya Dmitrenok <dmitrenokdarya@gmail.com>
2025-09-28 17:07:03 +0300

    KAN-21 feat: add custom alert message story

--- src/shared/components/Alert/Alert.stories.tsx
+++ src/shared/components/Alert/Alert.stories.tsx
@@ -15,15 +15,24 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-    args: {
-
-    },
     render: () => {
         return (
             <div>
                 <Alert />
                 <Button onClick={() => alert.success()} className='mr-4 bg-success-700'>Success :)</Button>
                 <Button onClick={() => alert.error()} className='bg-danger-700'>Error :(</Button>
+            </div>
+        )
+    },
+}
+
+export const CustomAlertMessage: Story = {
+    render: () => {
+        return (
+            <div>
+                <Alert />
+                <Button onClick={() => alert.success('Кастомное сообщение о каком-то успехе')} className='mr-4 bg-success-700'>Success :)</Button>
+                <Button onClick={() => alert.error('Кастомное сообщение о какой-то ошибке')} className='bg-danger-700'>Error :(</Button>
             </div>
         )
     },
