ac2375a18b2ccbd26ac0cf06c405f18ee25ae4f8
Filipp Sam <philippsm666@gmail.com>
2025-10-04 10:27:10 +0300

    KAN-56 fix: clx changed by cn

--- src/shared/components/PageContainer/PageContainer.tsx
+++ src/shared/components/PageContainer/PageContainer.tsx
@@ -1,13 +1,13 @@
 'use client'
 import { type ComponentPropsWithRef } from 'react'
-import { clsx } from 'clsx'
+import { cn } from '@/shared/lib'
 
 type Props = ComponentPropsWithRef<'main'> & {
    className?: string
 }
 
 export function PageContainer({ className, ...rest }: Props) {
-   const classNames = clsx('mx-auto w-full', 'max-w-[1280px] py-6 px-[60px]', className)
-
-   return <main className={classNames} {...rest} />
+   return (
+      <main className={cn('mx-auto w-full max-w-[1280px] px-[60px] py-6', className)} {...rest} />
+   )
 }
