92eaeb4c8aea41b14266c6303c98e04b3d02d131
Evgeniy Ivanov <p8635241@gmail.com>
2025-10-12 19:14:57 +0500

    KAN-62 refactor: isErrorWithMessage.ts refactor from array to object

--- src/shared/utils/isErrorWithMessage.ts
+++ src/shared/utils/isErrorWithMessage.ts
@@ -1,13 +1,8 @@
 export function isErrorWithMessage(error: unknown): error is { message: string; field: string }[] {
    return (
-      typeof error === 'object' &&
-      Array.isArray(error) &&
-      error.length === 1 &&
-      error[0].message &&
-      error[0].field
-      // typeof error === 'object' && // Проверяем, что error – это объект
-      // error != null && // Убеждаемся, что это не null
-      // 'message' in error && // Проверяем, что у объекта есть свойство 'message'
-      // typeof error.message === 'string' // Убеждаемся, что 'message' это строка
+      typeof error === 'object' && // Проверяем, что error – это объект
+      error != null && // Убеждаемся, что это не null
+      'message' in error && // Проверяем, что у объекта есть свойство 'message'
+      typeof error.message === 'string' // Убеждаемся, что 'message' это строка
    )
 }
