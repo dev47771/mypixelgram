b795260cf9b153ed90f98d21c5da6098657dc558
Darya Dmitrenok <dmitrenokdarya@gmail.com>
2025-10-16 21:19:20 +0300

    KAN-49 fix: add Script component from next/script

--- src/app/(auth)/verification-expired/layout.tsx
+++ src/app/(auth)/verification-expired/layout.tsx
@@ -1,13 +1,15 @@
 'use client'
 
+import Script from 'next/script'
 import { type ReactNode } from 'react'
 
 export default function ForgotPasswordLayout({ children }: { children: ReactNode }) {
    return (
       <>
-         <script
+         <Script
             async
             src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`}
+            strategy="afterInteractive"
          />
          {children}
       </>
