411134b2d1a3291312657c6ec3e4dbd3ae21cf84
Darya Dmitrenok <dmitrenokdarya@gmail.com>
2025-10-16 18:05:48 +0300

    fix/KAN-50 style: center Loader component

--- src/shared/components/Loader/Loader.tsx
+++ src/shared/components/Loader/Loader.tsx
@@ -6,26 +6,28 @@ type Props = {
 
 export function Loader({ size = '2.8rem', color = '#397DF6', speed = 0.9 }: Props) {
    return (
-      <div
-         className="relative flex items-center justify-start"
-         style={{ height: size, width: size }}
-      >
-         {[...Array(8)].map((_, index) => (
-            <div
-               key={index}
-               className="absolute top-0 left-0 flex h-full w-full items-center justify-start"
-               style={{ transform: `rotate(${index * 45}deg)` }}
-            >
+      <div className="fixed inset-0 flex items-center justify-center">
+         <div
+            className="relative flex items-center justify-center"
+            style={{ height: size, width: size }}
+         >
+            {[...Array(8)].map((_, index) => (
                <div
-                  className="animate-pulse-scale h-1/5 w-1/5 rounded-full opacity-50 shadow-lg"
-                  style={{
-                     backgroundColor: color,
-                     animationDelay: `${-speed * (index * 0.125)}s`,
-                     boxShadow: `0 0 20px ${color}30`,
-                  }}
-               />
-            </div>
-         ))}
+                  key={index}
+                  className="absolute top-0 left-0 flex h-full w-full items-center justify-start"
+                  style={{ transform: `rotate(${index * 45}deg)` }}
+               >
+                  <div
+                     className="animate-pulse-scale h-1/5 w-1/5 rounded-full opacity-50 shadow-lg"
+                     style={{
+                        backgroundColor: color,
+                        animationDelay: `${-speed * (index * 0.125)}s`,
+                        boxShadow: `0 0 20px ${color}30`,
+                     }}
+                  />
+               </div>
+            ))}
+         </div>
       </div>
    )
 }
