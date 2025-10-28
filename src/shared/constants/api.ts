f994dc3d05e066d2ed3e7c95d0db8193fe3bd277
FilippSM <56367648+FilippSM@users.noreply.github.com>
2025-10-28 12:54:02 +0300

    YAY-5 feat: add const loginGutHub in apimap

--- src/shared/constants/api.ts
+++ src/shared/constants/api.ts
@@ -4,4 +4,5 @@ export const TOKEN = 'accessToken'
 
 export const apiMap = {
    loginGoogle: `${process.env.NEXT_PUBLIC_BASE_URL}${AuthEndpoints.loginGoogle}`,
+   loginGitHub: `${process.env.NEXT_PUBLIC_BASE_URL}${AuthEndpoints.loginGitHub}`,
 }
