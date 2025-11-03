add79922941cc94af43f301f456e183dfe0382f3
Filipp Sam <philippsm666@gmail.com>
2025-11-04 02:03:35 +0300

    YAY-25 feat: add block in main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,24 @@
-export default function Home() {}
+import { PageContainer } from '@/shared/components/PageContainer'
+
+const arr = ['1', '2', '3', '4']
+
+type CardProps = {
+   children?: React.ReactNode
+}
+
+export default function Home() {
+   return (
+      <PageContainer>
+         <div className="bg-dark-500 border-dark-300 mb-[36px] h-[72px] w-[972px] border"></div>
+         <div className="flex flex-row gap-4">
+            {arr.map((el, index) => (
+               <Card key={index}>{el}</Card>
+            ))}
+         </div>
+      </PageContainer>
+   )
+}
+
+const Card = ({ children }: CardProps) => {
+   return <div className="bg-dark-500 border-dark-300 h-[391px] w-[234px] border">{children}</div>
+}
