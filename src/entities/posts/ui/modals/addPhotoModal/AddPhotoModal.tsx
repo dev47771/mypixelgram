0db44d84298d255fc51bba122abbbf30348ea088
Rokkcoco <p8635241@gmail.com>
2025-11-06 14:27:53 +0500

    YAY-12 refactor: addPhotoModal logic with navbar and post creator

--- src/entities/posts/ui/modals/addPhotoModal/AddPhotoModal.tsx
+++ src/entities/posts/ui/modals/addPhotoModal/AddPhotoModal.tsx
@@ -1,12 +1,11 @@
-import { Modal, ModalBody, ModalClose, ModalTitle } from '@/shared/components/Modal'
-import { Typography } from '@/shared/components/Typography'
-import { CrossIcon, PostOutlineIcon } from '@/shared/icons'
+import { PostOutlineIcon } from '@/shared/icons'
 import { Button } from '@/shared/components/Button'
 import { ChangeEvent, useEffect, useRef } from 'react'
 import { z } from 'zod'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { ACCEPTED_IMAGE_TYPES, imgSchema } from '@/shared/schema'
+import { PostModal } from '@/shared/components/PostModal'
 
 const schema = z.object({
    postPhoto: imgSchema('postPhoto').shape['postPhoto'],
@@ -61,51 +60,50 @@ export const AddPhotoModal = ({ isOpen, onPhotoSelected, onClose, onOpenChange }
    }
 
    return (
-      <Modal className={'w-[492px]'} open={isOpen} onOpenChange={onOpenChange}>
-         <ModalTitle className={'flex items-center justify-between'}>
-            <Typography variant={'h1'}>Add Photo</Typography>
-            <ModalClose asChild>
-               <CrossIcon />
-            </ModalClose>
-         </ModalTitle>
-         <hr className={'text-dark-100 h-[1px]'} />
-         <ModalBody className="flex flex-col items-center">
-            {errors.postPhoto?.message && (
-               <div
-                  className={`border-danger-500 bg-danger-900 leading-m text-light-100 text-m my-1.5 flex w-[445px] items-center justify-center rounded-xs border px-10 py-1.25 font-normal`}
-               >
-                  {
-                     <span className={'text-center'}>
-                        <span className={'font-bold'}>Error! </span>
-                        {errors.postPhoto?.message}
-                     </span>
-                  }
-               </div>
-            )}
+      <PostModal
+         open={isOpen}
+         onOpenChange={onOpenChange}
+         size="image-upload"
+         headerText="Add Photo"
+         headerVariant="close-only"
+         contentColumns="one"
+         className={'flex flex-col items-center'}
+      >
+         {errors.postPhoto?.message && (
             <div
-               className={`bg-dark-500 ${errors.postPhoto?.message ? '' : 'mt-[72px]'} mb-15 flex h-[228px] w-[222px] items-center justify-center rounded-xs`}
+               className={`border-danger-500 bg-danger-900 leading-m text-light-100 text-m my-1.5 flex w-[445px] items-center justify-center rounded-xs border px-10 py-1.25 font-normal`}
             >
-               <PostOutlineIcon className={'h-12 w-12'} />
+               {
+                  <span className={'text-center'}>
+                     <span className={'font-bold'}>Error! </span>
+                     {errors.postPhoto?.message}
+                  </span>
+               }
             </div>
-            <form className={'flex flex-col'}>
-               <div className={'flex max-w-[219px] flex-col gap-6'}>
-                  <Button type="button" onClick={addPhotoButtonHandler}>
-                     Select from Computer
-                  </Button>
-                  <Button type="button" variant={'outlined'} className={'mb-[48px]'}>
-                     Open Draft
-                  </Button>
-               </div>
-               <input
-                  type="file"
-                  ref={postPhotoRef}
-                  accept={ACCEPTED_IMAGE_TYPES.join(',')}
-                  className={'hidden'}
-                  onChange={fileLoaderHandler}
-                  {...restPostPhoto}
-               />
-            </form>
-         </ModalBody>
-      </Modal>
+         )}
+         <div
+            className={`bg-dark-500 ${errors.postPhoto?.message ? '' : 'mt-[72px]'} mb-15 flex h-[228px] w-[222px] items-center justify-center rounded-xs`}
+         >
+            <PostOutlineIcon className={'h-12 w-12'} />
+         </div>
+         <form className={'flex flex-col'}>
+            <div className={'flex max-w-[219px] flex-col gap-6'}>
+               <Button type="button" onClick={addPhotoButtonHandler}>
+                  Select from Computer
+               </Button>
+               <Button type="button" variant={'outlined'} className={'mb-[48px]'}>
+                  Open Draft
+               </Button>
+            </div>
+            <input
+               type="file"
+               ref={postPhotoRef}
+               accept={ACCEPTED_IMAGE_TYPES.join(',')}
+               className={'hidden'}
+               onChange={fileLoaderHandler}
+               {...restPostPhoto}
+            />
+         </form>
+      </PostModal>
    )
 }
