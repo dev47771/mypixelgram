97e5693493ad7ae5c1e70b4e7233d349c8ffc166
Darya Dmitrenok <dmitrenokdarya@gmail.com>
2025-11-18 18:22:10 +0300

    YAY-60 refactor: update PublicationModal props and add photo processing flow

--- src/entities/posts/ui/modals/PublicationModal/PublicationModal.tsx
+++ src/entities/posts/ui/modals/PublicationModal/PublicationModal.tsx
@@ -1,13 +1,17 @@
 'use client'
 
+import { PhotoState } from '@/features/post-creator/PostCreator'
+import { applyFilterToImage } from '@/features/post-creator/utils/applyImageFilter'
 import { PublicationForm, PublicationFormData } from '@/features/posts/forms/PublicationForm'
 import { usePublishPost } from '@/features/posts/hooks'
 import { Modal } from '@/shared/components/Modal'
 import { isErrorInDataResponse } from '@/shared/utils/typeguards/isErrorInDataResponse'
+import { alert } from '@/shared/components/Alert'
+import { ErrorResponse } from '@/features/auth/api'
 
 type Props = {
    onBack: () => void
-   photos: File[]
+   photos: PhotoState[]
    onClose: () => void
 }
 
@@ -15,18 +19,33 @@ export const PublicationModal = ({ onBack, photos, onClose }: Props) => {
    const { publishPost, isLoading, error } = usePublishPost()
 
    //mock data for uploadFile
-   photos = [
-      new File([], 'placeholder1.jpg'),
-      new File([], 'placeholder2.png'),
-      new File([], 'placeholder3.png'),
-   ]
+   // photos = [
+   //    new File([], 'placeholder1.jpg'),
+   //    new File([], 'placeholder2.png'),
+   //    new File([], 'placeholder3.png'),
+   // ]
 
    //mock data for slider
-   const images = ['./public/404.jpg', './public/logo-light.png', './public/logo-dark.png']
+   //const images = ['./public/404.jpg', './public/logo-light.png', './public/logo-dark.png']
+
+   // const handlePublish = async (dataPostData: PublicationFormData) => {
+   //    await publishPost(dataPostData, photos)
+   //    onClose()
+   // }
 
    const handlePublish = async (dataPostData: PublicationFormData) => {
-      await publishPost(dataPostData, photos)
-      onClose()
+      try {
+         // Ждём обработки всех фото
+         const processedPhotos = await Promise.all(photos.map(photo => applyFilterToImage(photo)))
+
+         // Отправка данных на сервер вместе с обработанными фото
+         await publishPost(dataPostData, processedPhotos)
+
+         // Закрываем модалку после успешной публикации
+         onClose()
+      } catch (error) {
+         alert.error((error as ErrorResponse).errorsMessages[0].message || 'Something went wrong')
+      }
    }
 
    return (
@@ -36,7 +55,7 @@ export const PublicationModal = ({ onBack, photos, onClose }: Props) => {
             onBack={onBack}
             errorsFromApi={isErrorInDataResponse(error) ? error?.data.errorsMessages : undefined}
             isLoading={isLoading}
-            images={images}
+            images={photos.map(photo => photo.previewUrl)}
          />
       </Modal>
    )
