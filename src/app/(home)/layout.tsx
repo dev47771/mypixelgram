5d17fe190ba53ce777bc903e5bdd7c4f5e263414
Filipp Sam <philippsm666@gmail.com>
2025-11-19 21:44:28 +0300

    YAY-50 fix: change error on data

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -5,21 +5,19 @@ import { cn } from '@/shared/lib'
 import { Sidebar } from '@/widgets/Sidebar'
 
 export default function HomeLayout({ children }: { children: React.ReactNode }) {
-   const { error } = useMeQuery()
-
-   const isAuthorized = !(error && 'status' in error && error.status === 401)
+   const { data } = useMeQuery()
 
    return (
       <div
          className={cn(
             'mx-auto flex w-full max-w-[1280px] px-[60px]',
-            isAuthorized ? 'flex-col items-start' : 'justify-center'
+            data ? 'flex-col items-start' : 'justify-center'
          )}
       >
-         {isAuthorized ? <Sidebar /> : null}
+         {data ? <Sidebar /> : null}
          <div
             className={`border-dark-300 min-h-screen border-l transition-all duration-300 ${
-               isAuthorized ? 'ml-[162px]' : 'ml-0 flex justify-center border-none'
+               data ? 'ml-[162px]' : 'ml-0 flex justify-center border-none'
             }`}
          >
             {children}
