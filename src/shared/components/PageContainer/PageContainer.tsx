e942820b238ddb5ba7504c38b9aa7c807af29c11
Filipp Sam <philippsm666@gmail.com>
2025-11-20 22:57:36 +0300

    YAY-50 feat: add useContext, change style

--- src/shared/components/PageContainer/PageContainer.tsx
+++ src/shared/components/PageContainer/PageContainer.tsx
@@ -4,6 +4,7 @@ import { type ComponentPropsWithRef } from 'react'
 import { cn } from '@/shared/lib'
 import { PublicRoutes } from '@/shared/enums'
 import { usePathname } from 'next/navigation'
+import { useAuth } from '@/app/(home)/layout'
 
 type Props = ComponentPropsWithRef<'main'> & {
    className?: string
@@ -13,6 +14,7 @@ const PUBLIC_ROUTES = Object.values(PublicRoutes) as string[]
 
 export function PageContainer({ className, ...rest }: Props) {
    const pathname = usePathname()
+   const { isAuthorized } = useAuth()
    const isPublic = PUBLIC_ROUTES.includes(pathname)
    const isHomePage = pathname === PublicRoutes.main
 
@@ -21,7 +23,7 @@ export function PageContainer({ className, ...rest }: Props) {
          className={cn(
             'mx-auto flex w-full max-w-[1280px] flex-col items-center px-[60px] pt-6',
             isPublic ? '' : 'pr-[0px] pl-[24px]',
-            isHomePage ? 'pr-[0px] pr-[24px] pl-[24px]' : '',
+            isHomePage && (isAuthorized ? 'pr-[0px] pl-[24px]' : 'pr-0 pl-0'),
             className
          )}
          {...rest}
