c2c42f51c5ffd25d299c4a227648633f2df47da4
Vika <vikapetrova8@gmail.com>
2025-11-23 19:02:47 +0300

    YAY-24 fix: use BASE_URL variable in apiMap

--- src/shared/constants/api.ts
+++ src/shared/constants/api.ts
@@ -1,10 +1,11 @@
 import { AuthEndpoints, PostsEndpoints, UserEndpoints } from '../enums'
 
 export const TOKEN = 'accessToken'
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 
 export const apiMap = {
-   loginGoogle: `${process.env.NEXT_PUBLIC_BASE_URL}${AuthEndpoints.loginGoogle}`,
-   loginGitHub: `${process.env.NEXT_PUBLIC_BASE_URL}${AuthEndpoints.loginGitHub}`,
-   lastPosts: `${process.env.NEXT_PUBLIC_BASE_URL}${PostsEndpoints.lastPosts}`,
-   usersTotalCount: `${process.env.NEXT_PUBLIC_BASE_URL}${UserEndpoints.usersTotalCount}`,
+   loginGoogle: `${BASE_URL}${AuthEndpoints.loginGoogle}`,
+   loginGitHub: `${BASE_URL}${AuthEndpoints.loginGitHub}`,
+   lastPosts: `${BASE_URL}${PostsEndpoints.lastPosts}`,
+   usersTotalCount: `${BASE_URL}${UserEndpoints.usersTotalCount}`,
 }
