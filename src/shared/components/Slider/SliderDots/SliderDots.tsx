38f501264b2f1d2c674f31a9232371be20f728e6
Darya Dmitrenok <dmitrenokdarya@gmail.com>
2025-11-27 02:13:25 +0300

    YAY-60 fix: add button type to SliderDots

--- src/shared/components/Slider/SliderDots/SliderDots.tsx
+++ src/shared/components/Slider/SliderDots/SliderDots.tsx
@@ -36,6 +36,7 @@ export const SliderDots = ({
                return (
                   <button
                      key={idx}
+                     type="button"
                      onClick={() => {
                         onDotClick(idx)
                      }}
