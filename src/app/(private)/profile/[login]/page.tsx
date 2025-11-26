19e80ceaf1dbe8b347e4b6a3547b94cf2e5d9dcc
Anton Barashkov <spone161@gmail.com>
2025-11-27 02:52:05 +0300

    YAY-22 fix: hide sidebar for unauthorized users on profile page

--- src/app/(private)/profile/[login]/page.tsx
+++ src/app/(private)/profile/[login]/page.tsx
@@ -7,7 +7,7 @@ export default function ProfilePage() {
    const { login } = useParams<{ login: string }>()
 
    return (
-      <div className={'flex flex-col pt-[36px] pl-6'}>
+      <div className={'flex w-full flex-col pt-[36px] pl-6'}>
          <ProfileHeader login={login} />
          <ProfilePosts login={login} />
       </div>
--- src/app/(private)/profile/layout.tsx
+++ src/app/(private)/profile/layout.tsx
@@ -1,14 +1,27 @@
+'use client'
+
 import { Sidebar } from '@/widgets/Sidebar'
 import { ReactNode } from 'react'
 import { PageContainer } from '@/shared/components/PageContainer'
+import { useMeQuery } from '@/features/auth/api'
+import { cn } from '@/shared/lib'
 
 export default function ProfileLayout({ children }: { children: ReactNode }) {
+   const { data: me } = useMeQuery()
+
    return (
       <PageContainer
-         className={'mx-auto w-full flex-row items-stretch justify-between px-[60px] py-0'}
+         className={'mx-auto w-full flex-row items-stretch justify-center px-[60px] py-0'}
       >
-         <Sidebar />
-         <div className="border-dark-300 ml-[162px] min-h-screen w-full border-l">{children}</div>
+         {me && <Sidebar />}
+         <div
+            className={cn(
+               'border-dark-300 ml-[162px] min-h-screen w-full border-l',
+               me ? 'ml-[162px]' : 'ml-0 flex max-w-[992px] justify-center border-none'
+            )}
+         >
+            {children}
+         </div>
       </PageContainer>
    )
 }
