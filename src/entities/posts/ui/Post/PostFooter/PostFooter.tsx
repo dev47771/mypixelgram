9c11d8341e9a2a815defe1bb553203cfaf921c1e
Vika <vikapetrova8@gmail.com>
2025-11-27 00:26:51 +0300

    YAY-17 fix: change props in PostFooter

--- src/entities/posts/ui/Post/PostFooter/PostFooter.tsx
+++ src/entities/posts/ui/Post/PostFooter/PostFooter.tsx
@@ -1,10 +1,10 @@
 import { PostActions } from '@/entities/posts/ui/Post/PostFooter/PostActions'
 import { PostStats } from '@/entities/posts/ui/Post/PostFooter/PostStats'
 import { PostAddComment } from '@/entities/posts/ui/Post/PostFooter/PostAddComment'
-import type { Post as PostType } from '@/entities/posts/model'
+import type { PostByIdType } from '@/features/posts/api'
 
 type Props = {
-   post: PostType
+   post: PostByIdType
 }
 export const PostFooter = ({ post }: Props) => {
    return (
@@ -12,8 +12,8 @@ export const PostFooter = ({ post }: Props) => {
          <div className={'border-dark-100 border-b px-6 pt-3 pb-2'}>
             <PostActions />
             <PostStats
-               userAvatar={post.userAvatar}
-               dateCreated={post.dateCreated}
+               userAvatar={post.user.avatar}
+               dateCreated={post.createdAt}
                likesCount={post.likesCount}
             />
          </div>
