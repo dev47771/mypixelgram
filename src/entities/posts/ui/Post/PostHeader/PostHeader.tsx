84b5240b88e5d931da51df3608646810c45aac4e
Vika <vikapetrova8@gmail.com>
2025-11-27 00:12:01 +0300

    YAY-17 fix: change props in PostHeader

--- src/entities/posts/ui/Post/PostHeader/PostHeader.tsx
+++ src/entities/posts/ui/Post/PostHeader/PostHeader.tsx
@@ -2,17 +2,17 @@ import { Typography } from '@/shared/components/Typography'
 import { PostMenu } from '@/entities/posts/ui/Post/PostHeader/PostMenu'
 
 import { Avatar } from '@/shared/components/Avatar'
-import type { PostByIdType } from '@/features/posts/api'
+import type { UserType } from '@/entities/user'
 
 type Props = {
-   post: PostByIdType
+   user: UserType
 }
 
-export const PostHeader = ({ post }: Props) => {
+export const PostHeader = ({ user }: Props) => {
    return (
       <div className={'border-dark-100 flex items-center gap-3 border-b px-6 py-[11px]'}>
-         <Avatar src={post.user.avatar} size={'sm'} />
-         <Typography variant={'h3'}>{post.user.login}</Typography>
+         <Avatar src={user.avatar} size={'sm'} />
+         <Typography variant={'h3'}>{user.login}</Typography>
          <PostMenu isOwner />
       </div>
    )
