2ccf4b20ba97fb5f6df0c6138add5e88e5e101ec
Vika <vikapetrova8@gmail.com>
2025-11-27 12:12:17 +0300

    YAY-17 fix: fix Post stories

--- src/entities/posts/ui/Post/Post.stories.tsx
+++ src/entities/posts/ui/Post/Post.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, StoryObj } from '@storybook/nextjs-vite'
 import { Post } from './Post'
+import type { PostByIdType } from '@/features/posts/api'
 
 const meta = {
    title: 'Post',
@@ -12,38 +13,50 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-const mockComment = {
-   userAvatar:
-      'https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png',
-   userName: 'UserName',
-   text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-   date: '2 Hours ago',
-}
+// const mockComment = {
+//    userAvatar:
+//       'https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png',
+//    userName: 'UserName',
+//    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+//    date: '2 Hours ago',
+// }
 
-const mockPost = {
-   id: '1',
-   userId: '23',
+const mockPost: PostByIdType = {
+   postId: '1',
+   user: {
+      id: '23',
+      login: 'hottabych',
+      avatar:
+         'https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png',
+   },
    images: [
-      'https://i.pinimg.com/736x/be/39/7c/be397c91b8026b17f5f8a6ed98e23e9e.jpg',
-      'https://cdn.fishki.net/upload/post/2021/02/16/3613245/tn/alberta-2297204-1280.jpg',
-      'https://kulturologia.ru/files/u18172/181725398.jpg',
-   ],
-
-   userAvatar:
-      'https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png',
-   userName: 'UserName',
-   description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-   date: '2 Hours ago',
-   comments: [
-      mockComment,
       {
-         ...mockComment,
-         likes: 1,
+         url: 'https://i.pinimg.com/736x/be/39/7c/be397c91b8026b17f5f8a6ed98e23e9e.jpg',
+         fileId: '1',
+      },
+      {
+         url: 'https://cdn.fishki.net/upload/post/2021/02/16/3613245/tn/alberta-2297204-1280.jpg',
+         fileId: '2',
+      },
+      {
+         url: 'https://kulturologia.ru/files/u18172/181725398.jpg',
+         fileId: '3',
       },
    ],
+   description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+   updatedAt: '2 Hours ago',
+   // comments: [
+   //    mockComment,
+   //    {
+   //       ...mockComment,
+   //       likes: 1,
+   //    },
+   // ],
    likesCount: 2243,
-   dateCreated: 'July 3, 2021',
+   createdAt: 'July 3, 2021',
+   location: null,
+   userLikeStatus: 'None',
 }
 
 export const Default: Story = {
