c94bcbc881bb0ddf78ed4e3373c0ad41584c2f5f
Vika <vikapetrova8@gmail.com>
2025-11-27 17:08:13 +0300

    YAY-17 fix: add DeletePostModal on delete post

--- src/entities/posts/ui/Post/PostHeader/PostMenu/PostMenu.tsx
+++ src/entities/posts/ui/Post/PostHeader/PostMenu/PostMenu.tsx
@@ -4,6 +4,7 @@ import { DropDownMenuTrigger } from '@/shared/components/DropDownMenu/DropDownMe
 import { Typography } from '@/shared/components/Typography'
 import { POST_MENU_ITEMS } from '@/entities/posts/ui/Post/PostHeader/PostMenu/config'
 import { usePostMenuActions } from './usePostMenuActions'
+import { DeletePostModal } from '@/entities/posts/ui/modals/DeletePostModal/DeletePostModal'
 
 type Props = {
    isOwner: boolean
@@ -12,29 +13,38 @@ type Props = {
 
 export const PostMenu = ({ isOwner, postId }: Props) => {
    const dropDownItems = isOwner ? POST_MENU_ITEMS.owner : POST_MENU_ITEMS.viewer
-   const { handleAction } = usePostMenuActions(postId)
+   const { handleAction, isDeleteModalOpen, confirmDelete, closeDeletePostModal } =
+      usePostMenuActions(postId)
 
    return (
-      <DropDownMenu
-         trigger={
-            <DropDownMenuTrigger className={'data-[state=open]:text-accent-500 ml-auto'}>
-               <MoreIcon />
-            </DropDownMenuTrigger>
-         }
-         alignOffset={0}
-      >
-         {dropDownItems.map(({ icon: Icon, value, action }) => (
-            <DropDownMenuItem
-               key={value}
-               className={'flex items-center gap-3 p-3'}
-               onSelect={() => handleAction(action)}
-            >
-               <Icon />
-               <Typography variant={'captionRegular'} as={'span'}>
-                  {value}
-               </Typography>
-            </DropDownMenuItem>
-         ))}
-      </DropDownMenu>
+      <>
+         <DropDownMenu
+            trigger={
+               <DropDownMenuTrigger className={'data-[state=open]:text-accent-500 ml-auto'}>
+                  <MoreIcon />
+               </DropDownMenuTrigger>
+            }
+            alignOffset={0}
+         >
+            {dropDownItems.map(({ icon: Icon, value, action }) => (
+               <DropDownMenuItem
+                  key={value}
+                  className={'flex items-center gap-3 p-3'}
+                  onSelect={() => handleAction(action)}
+               >
+                  <Icon />
+                  <Typography variant={'captionRegular'} as={'span'}>
+                     {value}
+                  </Typography>
+               </DropDownMenuItem>
+            ))}
+         </DropDownMenu>
+
+         <DeletePostModal
+            open={isDeleteModalOpen}
+            onConfirm={confirmDelete}
+            onCancel={closeDeletePostModal}
+         />
+      </>
    )
 }
--- src/entities/posts/ui/Post/PostHeader/PostMenu/usePostMenuActions.ts
+++ src/entities/posts/ui/Post/PostHeader/PostMenu/usePostMenuActions.ts
@@ -1,24 +1,36 @@
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import { useDeletePostMutation } from '@/features/posts/api'
+import { useState } from 'react'
 
 export const usePostMenuActions = (postId: string) => {
    const router = useRouter()
    const pathname = usePathname()
    const searchParams = useSearchParams()
 
+   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false)
    const [deletePost] = useDeletePostMutation()
+
+   const openDeletePostModal = () => setIsDeleteModalOpen(true)
+   const closeDeletePostModal = () => setIsDeleteModalOpen(false)
+
+   const confirmDelete = async () => {
+      try {
+         await deletePost(postId).unwrap()
+
+         const params = new URLSearchParams(searchParams.toString())
+         params.delete('postId')
+         router.push(pathname + '?' + params.toString())
+
+         closeDeletePostModal()
+      } catch (e) {
+         console.error('Failed to delete post', e)
+      }
+   }
 
    const handleAction = async (action: string) => {
       switch (action) {
          case 'delete':
-            try {
-               await deletePost(postId).unwrap()
-               const params = new URLSearchParams(searchParams.toString())
-               params.delete('postId')
-               router.push(pathname + '?' + params.toString())
-            } catch (e) {
-               console.error('Failed to delete post', e)
-            }
+            openDeletePostModal()
             break
 
          default:
@@ -26,5 +38,5 @@ export const usePostMenuActions = (postId: string) => {
       }
    }
 
-   return { handleAction }
+   return { handleAction, isDeleteModalOpen, confirmDelete, closeDeletePostModal }
 }
