552b077720cab1932ccef3c7f47d031e53404bd2
Filipp Sam <philippsm666@gmail.com>
2025-12-04 07:19:40 +0300

    YAY-79 feat: basic markup added

--- src/app/(private)/profile/[login]/settings/page.tsx
+++ src/app/(private)/profile/[login]/settings/page.tsx
@@ -1,3 +1,16 @@
 export default function ProfileSettingsPage() {
-   return <div>ProfileSettingsPage</div>
+   return (
+      <div className={'flex w-full flex-col pt-[36px] pl-6'}>
+         <div className={'border-dark-100 h-[36px] w-[972px] border'}>Tabs</div>
+         <div className={'flex flex-row'}>
+            <div className={'flex flex-col'}>
+               <div className={'border-dark-100 h-[192px] w-[192px] border'}>Avatar</div>
+               <div className={'border-dark-100 h-[36px] w-[201px] border'}>
+                  Select Profile photo
+               </div>
+            </div>
+            <div className={'border-dark-100 h-[700px] w-[740px] border'}>Form</div>
+         </div>
+      </div>
+   )
 }
