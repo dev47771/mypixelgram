2678d3249a7123fc7c94e70db2632df1977e5c6b
Filipp Sam <philippsm666@gmail.com>
2025-12-06 10:44:06 +0300

    YAY-79 feat: add avatarModal

--- src/app/(private)/profile/[login]/settings/page.tsx
+++ src/app/(private)/profile/[login]/settings/page.tsx
@@ -1,36 +1,46 @@
 'use client'
 
+import { AddAvatarModal } from '@/entities/settings/ui/modals/AddAvatarModal/AddAvatarModal'
 import { useUserProfile } from '@/entities/user/model/useUserProfile'
 import { Avatar } from '@/shared/components/Avatar'
 import { Button } from '@/shared/components/Button'
 import { PostOutlineIcon } from '@/shared/icons'
+import { useState } from 'react'
 
 export default function ProfileSettingsPage() {
+   const [isAvatarModalOpen, setIsAvatarModalOpen] = useState(false)
    const { userProfile } = useUserProfile()
 
+   const handleAvatarClick = () => setIsAvatarModalOpen(true)
+
    return (
-      <div className={'flex w-full flex-col pt-[36px] pl-6'}>
-         <div className={'border-dark-100 h-[36px] w-[972px] border'}>Tabs</div>
-         <div className={'flex flex-row'}>
-            <div className={'mt-[48px] mr-[38px] flex flex-col'}>
-               <div className={'mb-[24px] self-center'}>
-                  {userProfile?.user.avatar ? (
-                     <Avatar size="xl" src={userProfile?.user.avatar} alt="user avatar" />
-                  ) : (
-                     <div className="bg-dark-500 flex h-48 w-48 items-center justify-center rounded-full">
-                        <PostOutlineIcon width={48} height={48} />
-                     </div>
-                  )}
+      <>
+         <div className={'flex w-full flex-col pt-[36px] pl-6'}>
+            <div className={'border-dark-100 h-[36px] w-[972px] border'}>Tabs</div>
+            <div className={'flex flex-row'}>
+               <div className={'mt-[48px] mr-[38px] flex flex-col'}>
+                  <div className={'mb-[24px] self-center'}>
+                     {userProfile?.user.avatar ? (
+                        <Avatar size="xl" src={userProfile?.user.avatar} alt="user avatar" />
+                     ) : (
+                        <div className="bg-dark-500 flex h-48 w-48 items-center justify-center rounded-full">
+                           <PostOutlineIcon width={48} height={48} />
+                        </div>
+                     )}
+                  </div>
+                  <Button
+                     variant="outlined"
+                     className="max-w-[201px] items-center"
+                     onClick={handleAvatarClick}
+                  >
+                     Select Profile photo
+                  </Button>
                </div>
-               {/* <div className={'border-dark-100 h-[36px] w-[201px] border'}>
-                  Select Profile photo
-               </div> */}
-               <Button variant="outlined" className="max-w-[201px] items-center">
-                  Select Profile photo
-               </Button>
+               <div className={'border-dark-100 h-[700px] w-[740px] border'}>Form</div>
             </div>
-            <div className={'border-dark-100 h-[700px] w-[740px] border'}>Form</div>
          </div>
-      </div>
+
+         <AddAvatarModal open={isAvatarModalOpen} />
+      </>
    )
 }
---
+++ src/entities/settings/ui/modals/AddAvatarModal/AddAvatarModal.tsx
@@ -0,0 +1,108 @@
+import { PostOutlineIcon } from '@/shared/icons'
+import { Button } from '@/shared/components/Button'
+import { ChangeEvent, useEffect, useRef } from 'react'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { ACCEPTED_IMAGE_TYPES, imgSchema } from '@/shared/schema'
+import { PostModal } from '@/shared/components/PostModal'
+
+const schema = z.object({
+   postPhoto: imgSchema('postPhoto').shape['postPhoto'],
+})
+
+type FormTypes = z.infer<typeof schema>
+
+type Props = {
+   onPhotoSelected?: (file: File) => void
+   onOpenChange?: (value: boolean) => void
+   open: boolean
+}
+
+export const AddAvatarModal = ({ onPhotoSelected, onOpenChange, open }: Props) => {
+   const fileInputRef = useRef<HTMLInputElement>(null)
+
+   const {
+      register,
+      trigger,
+      formState: { errors },
+      watch,
+   } = useForm<FormTypes>({
+      defaultValues: { postPhoto: undefined },
+      resolver: zodResolver(schema),
+   })
+
+   const { onChange, ref, ...restPostPhoto } = register('postPhoto')
+
+   const postPhotoWatcher = watch('postPhoto')
+
+   useEffect(() => {
+      if (postPhotoWatcher && postPhotoWatcher.length > 0 && !errors.postPhoto) {
+         const file = postPhotoWatcher[0]
+         //onPhotoSelected(file)
+         onPhotoSelected?.(file)
+      }
+   }, [errors.postPhoto, onPhotoSelected, postPhotoWatcher])
+
+   const postPhotoRef = (e: HTMLInputElement | null) => {
+      ref(e)
+      fileInputRef.current = e
+   }
+
+   const fileLoaderHandler = (e: ChangeEvent<HTMLInputElement>) => {
+      void onChange(e)
+      void trigger('postPhoto')
+   }
+
+   const addPhotoButtonHandler = () => {
+      fileInputRef.current?.click()
+   }
+
+   return (
+      <PostModal
+         open={open}
+         onOpenChange={onOpenChange}
+         size="image-upload"
+         headerText="Add Photo"
+         headerVariant="close-only"
+         contentColumns="one"
+         className={'flex flex-col items-center'}
+      >
+         {errors.postPhoto?.message && (
+            <div
+               className={`border-danger-500 bg-danger-900 leading-m text-light-100 text-m my-1.5 flex w-[445px] items-center justify-center rounded-xs border px-10 py-1.25 font-normal`}
+            >
+               {
+                  <span className={'text-center'}>
+                     <span className={'font-bold'}>Error! </span>
+                     {errors.postPhoto?.message.toString()}
+                  </span>
+               }
+            </div>
+         )}
+         <div
+            className={`bg-dark-500 ${errors.postPhoto?.message ? '' : 'mt-[72px]'} mb-15 flex h-[228px] w-[222px] items-center justify-center rounded-xs`}
+         >
+            <PostOutlineIcon className={'h-12 w-12'} />
+         </div>
+         <form className={'flex flex-col'}>
+            <div className={'flex max-w-[219px] flex-col gap-6'}>
+               <Button type="button" onClick={addPhotoButtonHandler}>
+                  Select from Computer
+               </Button>
+               <Button type="button" variant={'outlined'} className={'mb-[48px]'}>
+                  Open Draft
+               </Button>
+            </div>
+            <input
+               type="file"
+               ref={postPhotoRef}
+               accept={ACCEPTED_IMAGE_TYPES.join(',')}
+               className={'hidden'}
+               onChange={fileLoaderHandler}
+               {...restPostPhoto}
+            />
+         </form>
+      </PostModal>
+   )
+}
