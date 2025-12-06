26dc666efd7729b395366676f9f9d3b8f11ef393
Rokkcoco <p8635241@gmail.com>
2025-12-06 14:04:48 +0500

    YAY-76 feat: change CSR to SSR on public and private pages

--- src/shared/hooks/useAuthSync.ts
+++ src/shared/hooks/useAuthSync.ts
@@ -6,13 +6,10 @@ import { authChannel } from '@/shared/lib/authBroadcast'
 import { useAppDispatch } from '@/shared/hooks/useAppDispatch'
 import { PrivateRoutes, PublicRoutes } from '@/shared/enums'
 import { authService } from '@/features/auth/api'
-import {baseApi} from "@/shared/store";
-
-
+import { baseApi } from '@/shared/store'
 
 export const useAuthSync = () => {
-
-    const router = useRouter()
+   const router = useRouter()
    const dispatch = useAppDispatch()
 
    useEffect(() => {
