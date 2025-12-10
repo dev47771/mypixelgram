88e5f7d9af17de9b80e4f96b288fad48fe1d95ee
Filipp Sam <philippsm666@gmail.com>
2025-12-10 07:23:36 +0300

    YAY-79 feat: add button on avatar

--- src/app/(private)/profile/[login]/settings/page.tsx
+++ src/app/(private)/profile/[login]/settings/page.tsx
@@ -4,8 +4,10 @@ import { AddAvatarModal } from '@/entities/settings/ui/modals/AddAvatarModal/Add
 import { useUserProfile } from '@/entities/user/model/useUserProfile'
 import { Avatar } from '@/shared/components/Avatar'
 import { Button } from '@/shared/components/Button'
-import { PostOutlineIcon } from '@/shared/icons'
+import { CrossIcon, PostOutlineIcon } from '@/shared/icons'
 import { useState } from 'react'
+import Image from 'next/image'
+import clsx from 'clsx'
 
 export default function ProfileSettingsPage() {
    const [isAvatarModalOpen, setIsAvatarModalOpen] = useState(false)
@@ -19,7 +21,7 @@ export default function ProfileSettingsPage() {
             <div className={'border-dark-100 h-[36px] w-[972px] border'}>Tabs</div>
             <div className={'flex flex-row'}>
                <div className={'mt-[48px] mr-[38px] flex flex-col'}>
-                  <div className={'mb-[24px] self-center'}>
+                  <div className={'mb-[24px] w-[201px] self-center'}>
                      {userProfile?.user.avatar ? (
                         <Avatar size="xl" src={userProfile?.user.avatar} alt="user avatar" />
                      ) : (
@@ -27,6 +29,29 @@ export default function ProfileSettingsPage() {
                            <PostOutlineIcon width={48} height={48} />
                         </div>
                      )}
+
+                     <div className="flex w-[201px] justify-center">
+                        <div className="relative">
+                           <Image
+                              src="/logo-light.png"
+                              alt="avatar"
+                              width={192}
+                              height={192}
+                              className="rounded-full"
+                           />
+
+                           <button
+                              className={clsx(
+                                 'absolute right-[8px] bottom-[155px] z-20 flex items-center justify-center',
+                                 'before:absolute before:h-6 before:w-6 before:rounded-full',
+                                 'before:border-dark-900 before:border-4 before:bg-red-500',
+                                 'cursor-pointer before:-z-10 before:box-border'
+                              )}
+                           >
+                              <CrossIcon className="h-4 w-4" />
+                           </button>
+                        </div>
+                     </div>
                   </div>
                   <Button
                      variant="outlined"
