fefa440f64d492a33fa9ebca27e1f41ab84bce08
Rokkcoco <p8635241@gmail.com>
2025-12-11 17:00:07 +0500

    YAY-81 feat: add pathname validation

--- src/entities/auth/model/NoAgeNotAllowed.tsx
+++ src/entities/auth/model/NoAgeNotAllowed.tsx
@@ -1,18 +1,19 @@
 'use client'
 import { useEffect } from 'react'
 import { useMeQuery } from '@/features/auth/api'
-import { useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import { PrivateRoutes } from '@/shared/enums'
 
 export const NoAgeNotAllowed = () => {
    const router = useRouter()
+   const pathname = usePathname()
    const { data: meData, isLoading } = useMeQuery()
 
    useEffect(() => {
       if (meData && !isLoading && meData.dateOfBirth === null) {
          router.push(PrivateRoutes.settings(meData.login))
       }
-   }, [isLoading, meData, router])
+   }, [isLoading, meData, router, pathname])
 
    return null
 }
