489df200f78fedf05d62f376005c6507176e3e49
Vika <vikapetrova8@gmail.com>
2025-12-13 00:13:56 +0300

    YAY-77 fix: add EditPostModal

--- src/entities/posts/ui/Post/PostClientWrapper/PostClientWrapper.tsx
+++ src/entities/posts/ui/Post/PostClientWrapper/PostClientWrapper.tsx
@@ -2,9 +2,15 @@
 import type { PostByIdType } from '@/features/posts/api'
 import { Post } from '@/entities/posts/ui/Post'
 import { usePostController } from '@/features/posts/hooks'
+import { EditPostModal } from '@/entities/posts/ui/modals/EditPostModal'
 
 export function PostClientWrapper({ post }: { post: PostByIdType }) {
-   const { closePostModal } = usePostController()
+   const { closePostModal, closeEditPostModal, isEditOpen } = usePostController()
 
-   return <Post post={post} onClose={closePostModal} />
+   return (
+      <>
+         <Post post={post} onClose={closePostModal} />
+         {isEditOpen && post && <EditPostModal post={post} onCloseAction={closeEditPostModal} />}
+      </>
+   )
 }
