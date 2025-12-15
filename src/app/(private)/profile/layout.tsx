215cfbe7257f942de01df63514af470542d11bb1
Vika <vikapetrova8@gmail.com>
2025-12-15 21:42:40 +0300

    fix: add layout for login page

--- src/app/(private)/profile/layout.tsx
+++ src/app/(private)/profile/layout.tsx
@@ -4,22 +4,11 @@ import { Sidebar } from '@/widgets/Sidebar'
 import { ReactNode } from 'react'
 import { PageContainer } from '@/shared/components/PageContainer'
 
-export default function ProfilePrivateLayout({
-   children,
-   post,
-}: {
-   children: ReactNode
-   post: ReactNode
-}) {
+export default function ProfilePrivateLayout({ children }: { children: ReactNode }) {
    return (
-      <PageContainer
-         className={'mx-auto w-full flex-row items-stretch justify-center px-[60px] py-0'}
-      >
+      <PageContainer className={'mx-auto w-full flex-row items-stretch justify-center px-15 py-0'}>
          <Sidebar />
-         <div className={'ml-[162px] w-full'}>
-            {children}
-            {post}
-         </div>
+         <div className={'ml-[162px] w-full'}>{children}</div>
       </PageContainer>
    )
 }
