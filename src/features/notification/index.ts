d035bab2356493c6f5d488c4a04a30bc16bde444
Darya Dmitrenok <dmitrenokdarya@gmail.com>
2026-02-17 22:58:22 +0300

    YAY-113 refactor: move Toast to shared

--- src/features/notification/index.ts
+++ src/features/notification/index.ts
@@ -1,4 +1,4 @@
 export * from './ui/ToastContainer'
-export * from './ui/Toast/Toast'
+export * from '../../shared/components/Toast/Toast'
 export * from './hooks/useToast'
 export * from './model/toast.types'
